0d2f43a414c82e1e16f476f37c9c730eb1dfa088
FihuraTaras <figura.taras@gmail.com>
2021-10-20 14:29:39 +0300

    tf

---
+++ Lab5.1/Lab5.1.cpp
@@ -0,0 +1,20 @@
+// Fihura Taras / IT-12
+// Lab 5_1
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+double f(const double x, const double y, const double z); // прототип
+int main()
+{
+	  double s, t;
+		cout << "s = "; cin >> s;
+	    cout << "t = "; cin >> t;
+      double c = 1.*(f(1,t+s,s) + f(t, s*t, 1)) / 1.*( 1+ (f*f)(s,1,t));
+	    cout << "c = " << c << endl;
+ return 0;
+}
+      double f(const double a, const double b, const double c) // визначення
+{
+		  return (a * sin(b) + b * sin(a) + c * c);
+}
